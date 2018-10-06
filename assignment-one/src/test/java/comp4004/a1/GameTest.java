c807054e3b2f1a268658ac0e87294d04e7aba0c6
enforser <trailcapital@gmail.com>
2018-10-06 13:51:41 -0400

    add test to see if one hand is better than other given they are not equal 'type'

---
+++ assignment-one/src/test/java/comp4004/a1/GameTest.java
@@ -0,0 +1,33 @@
+package comp4004.a1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+   @Test
+   public void isBetterHandTypes() {
+       Game testGame;
+
+       // initialized in order of winners vs. losers
+       Hand royalFlush = new Hand("royalFlush.txt");
+       Hand straightFlush = new Hand("straightFlush.txt");
+       Hand fourOfAKind = new Hand("handWithFourOfAKind.txt");
+       Hand fullHouse = new Hand("fullHouse.txt");
+       Hand flush = new Hand("flush.txt");
+       Hand straight = new Hand("unorderedStraight.txt");
+       Hand threeOfAKind = new Hand("handWithThreeOfAKind.txt");
+       Hand twoOfAKind = new Hand("handWithPairTogether.txt");
+       Hand highCard = new Hand("highestCard.txt");
+
+       // size of 9.
+       Hand[] orderedHands = {royalFlush, straightFlush, fourOfAKind, fullHouse, flush, straight, threeOfAKind, twoOfAKind, highCard};
+
+       for (int x = 1; x < orderedHands.length; x++) { // defines root hand
+           for (int y = x - 1; y >= 0; y--) { // compare all lower ranked hands to root hand
+              testGame = new Game(orderedHands[x], orderedHands[y]);
+              assertTrue(testGame.isBetter());
+           }
+       }
+   }
+}
