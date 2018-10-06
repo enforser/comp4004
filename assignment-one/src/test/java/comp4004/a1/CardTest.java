c0d3f33e200ed424389933610c5c7c5232dd76a1
enforser <trailcapital@gmail.com>
2018-10-06 16:16:40 -0400

    tests for if single card is better

--- assignment-one/src/test/java/comp4004/a1/CardTest.java
+++ assignment-one/src/test/java/comp4004/a1/CardTest.java
@@ -29,4 +29,20 @@ public class CardTest {
     public void suitAccess() {
         assertEquals(3, new Card("S", "A").suit);
     }
+
+    @Test
+    public void greaterThan() {
+        // same card => false
+        assertFalse(new Card("D", "A").greaterThan(new Card("D", "A")));
+        // lesser rank => false
+        assertFalse(new Card("D", "2").greaterThan(new Card("D", "3")));
+        // equal ranks but lesser suit => false
+        assertFalse(new Card("D", "A").greaterThan(new Card("S", "A")));
+        // greater suit and rank => true
+        assertTrue(new Card("S", "5").greaterThan(new Card("H", "4")));
+        // same rank but greater suit => true
+        assertTrue(new Card("S", "5").greaterThan(new Card("H", "5")));
+        // same suit but greater rank => true
+        assertTrue(new Card("S", "5").greaterThan(new Card("S", "4")));
+    }
 }
